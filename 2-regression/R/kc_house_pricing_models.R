176b1a35602321d986a4ef455ead7de777ea49e3
Martin Strandbygaard <martin@strandbygaard.net>
2016-04-09 09:52:59 +0200

    Added R implementation

--- 2-regression/R/kc_house_pricing_models.R
+++ 2-regression/R/kc_house_pricing_models.R
@@ -1,5 +1,34 @@
 train_data = read.csv("data/kc_house_train_data.csv")
-
 test_data = read.csv("data/kc_house_test_data.csv")
+train_data$date = as.Date(strptime(train_data$date, "%Y%m%dT%H%M%S"))
+test_data$date = as.Date(strptime(test_data$date, "%Y%m%dT%H%M%S"))
+
+hist(train_data$date, breaks=100)
+summary(train_data$date)
+
+features = c('sqft_living', 'bedrooms', 'bathrooms');
+
+example_model = lm(price ~ sqft_living + bedrooms + bathrooms, data=train_data)
+example_predictions = predict(example_model,newdata=train_data)
+example_predictions[1]
+
+
+
+train_data$bedrooms_squared = train_data$bedrooms^2;
+train_data['bed_bath_rooms'] = train_data['bedrooms'] * train_data['bathrooms'];
+train_data['log_sqft_living'] = train_data['sqft_living'].apply(lambda x: log(x));
+train_data['lat_plus_long'] = train_data['lat'] + train_data['long'];
+test_data$bedrooms_squared = test_data$bedrooms^2;
+test_data$bed_bath_rooms = test_data$bedrooms * test_data$bathrooms;
+test_data$log_sqft_living = log(test_data$sqft_living);
+test_data$lat_plus_long = test_data$lat + test_data$long;
+
+sums = test_data$bedrooms_squared + test_data$bed_bath_rooms + test_data$log_sqft_living + test_data$lat_plus_long;
+test_data
+sum(sums) / (4* nrow(test_data));
+
+
+
+
 
 
